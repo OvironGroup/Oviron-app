c7afa78d3ad03e77817e5b2b08e929b1a60da674
Ivan Lori <ivan.lori@protonmail.com>
2022-02-08 07:12:52 +0100

    style: format files with eslint

--- src/components/SidebarNav/SidebarNav.view.tsx
+++ src/components/SidebarNav/SidebarNav.view.tsx
@@ -1,14 +1,27 @@
-import { ReactElement } from 'react';
-import { Ocademy, Orchives } from '../../icons/icons';
-import styles from './SidebarNav.module.css';
+import React, { ReactElement } from 'react'
+import { Ocademy, Orchives } from '../../icons/icons'
+import styles from './SidebarNav.module.css'
 
 const SidebarNav = (): ReactElement => (
-	<aside className={`${styles.Container} bottom-0 w-full top-auto lg:top-16 lg:h-full lg:w-auto`}>
+	<aside
+		className={`${styles.Container} bottom-0 w-full top-auto lg:top-16 lg:h-full lg:w-auto`}
+	>
 		<ul className="p-0 m-0 flex justify-evenly lg:block">
 			<li>
 				<div className={`${styles.Home} ${styles.ToolsIcon}`}>
-					<svg xmlns="http://www.w3.org/2000/svg" width="23.442" height="21.267" viewBox="0 0 23.442 21.267">
-						<path id="Icon_metro-home" data-name="Icon metro-home" d="M26.012,15.669l-11.721-9.1-11.721,9.1V11.959l11.721-9.1,11.721,9.1Zm-2.93-.331v8.791h-5.86v-5.86h-5.86v5.86H5.5V15.338l8.791-6.593Z" transform="translate(-2.571 -2.861)" fill="#fff" />
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="23.442"
+						height="21.267"
+						viewBox="0 0 23.442 21.267"
+					>
+						<path
+							id="Icon_metro-home"
+							data-name="Icon metro-home"
+							d="M26.012,15.669l-11.721-9.1-11.721,9.1V11.959l11.721-9.1,11.721,9.1Zm-2.93-.331v8.791h-5.86v-5.86h-5.86v5.86H5.5V15.338l8.791-6.593Z"
+							transform="translate(-2.571 -2.861)"
+							fill="#fff"
+						/>
 					</svg>
 				</div>
 			</li>
@@ -24,6 +37,6 @@ const SidebarNav = (): ReactElement => (
 			</li>
 		</ul>
 	</aside>
-);
+)
 
-export default SidebarNav;
+export default SidebarNav
--- src/icons/icons.ts
+++ src/icons/icons.ts
@@ -31,5 +31,5 @@ export {
 	OvironSpaceWhite,
 	OvnetWhite,
 	ArrowLeft,
-	ArrowRight
+	ArrowRight,
 }
