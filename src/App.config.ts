8337b883b784bf42fe1d327ba965712e3ac9dcf9
Michael Trautner <trautna@gmail.com>
2022-03-02 18:40:49 +0100

    fix: updated scope to change metadata

--- src/App.config.ts
+++ src/App.config.ts
@@ -2,6 +2,7 @@ export const config = {
 	DOMAIN: 'oviron.eu.auth0.com',
 	BASE_API: 'https://oviron.eu.auth0.com/api/v2',
 	AUDIENCE: 'https://oviron.eu.auth0.com/api/v2/',
-	SCOPES: 'openid profile read:current_user',
+	SCOPES:
+		'openid profile read:current_user update:current_user_metadata',
 	CLIENT_ID: '5c0fjedvcbgRgUSxnY4C6njow7Ir5Vea',
 }
